9cb4426279793c06a374351cf220fad07db94265
Will Porter <willjsporter@hotmail.co.uk>
2017-10-02 13:51:49 +0100

    initialize working for bank

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,3 +1,10 @@
 class Bank
 
+attr_reader :balance, :transactions
+
+def initialize
+  @balance = 0
+  @transactions = Array.new
+end
+
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,17 +4,37 @@ describe Bank do
 
 let(:bank) { described_class.new }
 
-  context "#will store a balance" do
-    it "default balance is zero" do
+  context "#initializes with some attributes" do
+
+    it "initial balance is zero" do
       expect(bank.balance).to eq 0
     end
+
+    it "initializes with a blank transaction array" do
+      expect(bank.transactions.class).to eq Array
+      expect(bank.transactions.length).to eq 0
+    end
+
   end
 
   context "#clients can make deposits" do
-    
+    it "#balance increases after a deposit" do
+      bank.deposit(10)
+      expect(bank.balance).to eq 10
+    end
+
+    it "#transaction is stored in transaction array" do
+      expect(bank.transactions.length).to eq 2
+    end
+
   end
 
   context "#clients can make withdrawals" do
+
+    it "#balance decreases for withdrawals" do
+      bank.withdraw(10)
+      expect(bank.balance).to eq -10
+    end
 
   end
 
