6ecb9efe8d1c8a32e7ce95d4ab8c080b063e316c
Will Porter <willjsporter@hotmail.co.uk>
2017-10-03 13:06:22 +0100

    transaction initialize tests added (TDD)

---
+++ spec/transaction_spec.rb
@@ -0,0 +1,33 @@
+# require 'bank'
+require 'transaction'
+
+describe Transaction do
+
+  context '#Transaction is initized correctly' do
+
+    before(:each) do
+      transaction = Transaction.new(-10,110)
+    end
+
+    it "initializes with date" do
+      expect(transaction.date).to eq(/\d\d\/\d\d\/2\d\d\d/)
+    end
+
+    it "initializes with credit and debit amount (one of which is nil)" do
+      expect(transaction.debit).to be_nil
+      expect(transaction.credit).to eq 10
+    end
+
+    it "initializes with new balance" do
+      expect(transaction.balance).to eq 110
+    end
+
+  end
+
+  context '#Transaction is initized without arguments' do
+    it '#Transaction cannot initialize without transaction amount and balance' do
+      expect { Transaction.new }.to raise_error ArgumentError
+    end
+  end
+
+end
